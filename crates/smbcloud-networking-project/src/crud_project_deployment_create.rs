f3787879668c19a0b30b75e856e42dbd70928d30
Seto Elkahfi <hej@setoelkahfi.se>
2025-05-31 19:35:10 +0200

    cleanup

--- crates/smbcloud-networking-project/src/crud_project_deployment_create.rs
+++ crates/smbcloud-networking-project/src/crud_project_deployment_create.rs
@@ -35,37 +35,3 @@ pub async fn create(
         _ => Err(anyhow!("Something wrong.")),
     }
 }
-
-pub async fn update(
-    env: Environment,
-    project_id: i32,
-    deployment_id: i32,
-    payload: DeploymentPayload,
-) -> Result<Deployment> {
-    // Get current token
-    let token = get_smb_token(env).await?;
-
-    debug!("Current token: {}", token);
-
-    let url = format!(
-        "{}/{}",
-        build_project_deployment_index(env, project_id.to_string()),
-        deployment_id
-    );
-
-    let response = Client::new()
-        .put(url)
-        .json(&payload)
-        .header("Authorization", token)
-        .header("User-agent", SMB_USER_AGENT)
-        .send()
-        .await?;
-
-    match response.status() {
-        reqwest::StatusCode::OK => {
-            let deployment: Deployment = response.json().await?;
-            Ok(deployment)
-        }
-        _ => Err(anyhow!("Failed to update deployment.")),
-    }
-}
