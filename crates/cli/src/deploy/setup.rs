9f9a211d7ffc735561ad77db3ec634308135f85d
Seto Elkahfi <setoelkahfi@gmail.com>
2025-06-09 19:50:15 +0200

    remove config error

--- crates/cli/src/deploy/setup.rs
+++ crates/cli/src/deploy/setup.rs
@@ -1,17 +1,17 @@
-use crate::{
-    deploy::config::{Config, ConfigError},
-    ui::highlight,
-};
+use crate::{deploy::config::Config, ui::highlight};
 use dialoguer::{theme::ColorfulTheme, Confirm, Input, Select};
 use regex::Regex;
-use smbcloud_model::project::{Project, ProjectCreate};
+use smbcloud_model::{
+    error_codes::{ErrorCode, ErrorResponse},
+    project::{Project, ProjectCreate},
+};
 use smbcloud_networking::{environment::Environment, get_smb_token};
 use smbcloud_networking_project::{
     crud_project_create::create_project, crud_project_read::get_projects,
 };
 use std::{env, fs, path::Path};
 
-pub async fn setup_project(env: Environment) -> Result<Config, ConfigError> {
+pub async fn setup_project(env: Environment) -> Result<Config, ErrorResponse> {
     let path = env::current_dir().ok();
     let path_str = path
         .as_ref()
@@ -21,20 +21,36 @@ pub async fn setup_project(env: Environment) -> Result<Config, ConfigError> {
     let confirm = Confirm::with_theme(&ColorfulTheme::default())
         .with_prompt(format!("Setup project in {}? y/n", highlight(&path_str)))
         .interact()
-        .map_err(|_| ConfigError::InputError)?;
+        .map_err(|_| ErrorResponse::Error {
+            error_code: ErrorCode::InputError,
+            message: ErrorCode::InputError.message(None).to_string(),
+        })?;
 
     if !confirm {
-        return Err(ConfigError::Cancel);
+        return Err(ErrorResponse::Error {
+            error_code: ErrorCode::Cancel,
+            message: ErrorCode::Cancel.message(None).to_string(),
+        });
     }
 
     let access_token = match get_smb_token(env).await {
         Ok(token) => token,
-        Err(_) => return Err(ConfigError::MissingToken),
+        Err(_) => {
+            return Err(ErrorResponse::Error {
+                error_code: ErrorCode::Unauthorized,
+                message: ErrorCode::Unauthorized.message(None).to_string(),
+            })
+        }
     };
 
     let projects = match get_projects(env, access_token).await {
         Ok(x) => x,
-        Err(_) => return Err(ConfigError::InputError),
+        Err(_) => {
+            return Err(ErrorResponse::Error {
+                error_code: ErrorCode::InputError,
+                message: ErrorCode::InputError.message(None).to_string(),
+            })
+        }
     };
 
     let project: Project = if !projects.is_empty() {
@@ -56,12 +72,21 @@ pub async fn setup_project(env: Environment) -> Result<Config, ConfigError> {
     // Ensure .smb directory exists
     let smb_dir = Path::new(".smb");
     if !smb_dir.exists() {
-        fs::create_dir(smb_dir).map_err(|_| ConfigError::MissingConfig)?;
+        fs::create_dir(smb_dir).map_err(|_| ErrorResponse::Error {
+            error_code: ErrorCode::MissingConfig,
+            message: ErrorCode::MissingConfig.message(None).to_string(),
+        })?;
     }
 
     // Write config to .smb/config.toml
-    let config_toml = toml::to_string(&config).map_err(|_| ConfigError::MissingConfig)?;
-    fs::write(".smb/config.toml", config_toml).map_err(|_| ConfigError::MissingConfig)?;
+    let config_toml = toml::to_string(&config).map_err(|_| ErrorResponse::Error {
+        error_code: ErrorCode::MissingConfig,
+        message: ErrorCode::MissingConfig.message(None).to_string(),
+    })?;
+    fs::write(".smb/config.toml", config_toml).map_err(|_| ErrorResponse::Error {
+        error_code: ErrorCode::MissingConfig,
+        message: ErrorCode::MissingConfig.message(None).to_string(),
+    })?;
 
     Ok(config)
 }
@@ -70,11 +95,14 @@ async fn select_project(
     env: Environment,
     projects: Vec<Project>,
     path: &str,
-) -> Result<Project, ConfigError> {
+) -> Result<Project, ErrorResponse> {
     let confirm = Confirm::with_theme(&ColorfulTheme::default())
         .with_prompt("Use existing project? y/n")
         .interact()
-        .map_err(|_| ConfigError::InputError)?;
+        .map_err(|_| ErrorResponse::Error {
+            error_code: ErrorCode::InputError,
+            message: ErrorCode::InputError.message(None).to_string(),
+        })?;
 
     if !confirm {
         return create_new_project(env, path).await;
@@ -83,20 +111,23 @@ async fn select_project(
         .items(&projects)
         .default(0)
         .interact()
-        .map_err(|_| ConfigError::InputError)?;
+        .map_err(|_| ErrorResponse::Error {
+            error_code: ErrorCode::InputError,
+            message: ErrorCode::InputError.message(None).to_string(),
+        })?;
 
     let project = projects[selection].clone();
 
     Ok(project)
 }
 
-async fn create_new_project(env: Environment, path: &str) -> Result<Project, ConfigError> {
+async fn create_new_project(env: Environment, path: &str) -> Result<Project, ErrorResponse> {
     let default_name = Path::new(path)
         .file_name()
         .and_then(|os_str| os_str.to_str())
         .unwrap_or("project")
         .to_lowercase()
-        .replace(' ', "")
+        .replace([' ', '-'], "")
         .replace('-', "");
 
     let name = match Input::<String>::with_theme(&ColorfulTheme::default())
@@ -106,7 +137,10 @@ async fn create_new_project(env: Environment, path: &str) -> Result<Project, Con
     {
         Ok(project_name) => project_name,
         Err(_) => {
-            return Err(ConfigError::InputError);
+            return Err(ErrorResponse::Error {
+                error_code: ErrorCode::InputError,
+                message: ErrorCode::InputError.message(None).to_string(),
+            });
         }
     };
 
@@ -126,7 +160,10 @@ async fn create_new_project(env: Environment, path: &str) -> Result<Project, Con
     {
         Ok(repo) => repo,
         Err(_) => {
-            return Err(ConfigError::InputError);
+            return Err(ErrorResponse::Error {
+                error_code: ErrorCode::InputError,
+                message: ErrorCode::InputError.message(None).to_string(),
+            });
         }
     };
 
@@ -136,13 +173,21 @@ async fn create_new_project(env: Environment, path: &str) -> Result<Project, Con
     {
         Ok(description) => description,
         Err(_) => {
-            return Err(ConfigError::InputError);
+            return Err(ErrorResponse::Error {
+                error_code: ErrorCode::InputError,
+                message: ErrorCode::InputError.message(None).to_string(),
+            });
         }
     };
 
     let access_token = match get_smb_token(env).await {
         Ok(token) => token,
-        Err(_) => return Err(ConfigError::MissingToken),
+        Err(_) => {
+            return Err(ErrorResponse::Error {
+                error_code: ErrorCode::Unauthorized,
+                message: ErrorCode::Unauthorized.message(None).to_string(),
+            })
+        }
     };
 
     match create_project(
@@ -157,6 +202,6 @@ async fn create_new_project(env: Environment, path: &str) -> Result<Project, Con
     .await
     {
         Ok(project) => Ok(project),
-        Err(_) => Err(ConfigError::MissingConfig),
+        Err(e) => Err(e),
     }
 }
