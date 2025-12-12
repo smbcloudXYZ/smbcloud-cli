8c3a460da953217acb7b4eefe8172bd5ed51e678
Seto <setoelkahfi@gmail.com>
2025-12-12 15:19:09 +0100

    wip

--- crates/smbcloud-networking-account/src/oauth/get_consent_url.rs
+++ crates/smbcloud-networking-account/src/oauth/get_consent_url.rs
@@ -1,6 +1,25 @@
-pub fn get_consent_url(client_id: String, redirect_uri: String) -> String {
-    format!(
-        "https://accounts.google.com/o/oauth2/auth?client_id={}&redirect_uri={}&scope=email%20profile&response_type=code&access_type=offline",
-        client_id, redirect_uri
-    )
+use {crate::oauth::provider::Provider, uuid::Uuid};
+
+pub fn get_consent_url(provider: Provider) -> String {
+    match provider {
+        Provider::Google {
+            client_id,
+            redirect_uri,
+        } => format!(
+            "https://accounts.google.com/o/oauth2/auth?client_id={}&redirect_uri={}&scope=email%20profile&response_type=code&access_type=offline",
+            client_id, redirect_uri
+        ),
+        Provider::Apple {
+            client_id,
+            redirect_uri,
+            state,
+        } => {
+            let state_param = state.unwrap_or_else(|| Uuid::new_v4().to_string());
+
+            format!(
+                "https://appleid.apple.com/auth/authorize?client_id={}&redirect_uri={}&response_type=code&scope=email%20name&response_mode=form_post&state={}",
+                client_id, redirect_uri, state_param
+            )
+        }
+    }
 }
--- crates/smbcloud-networking-account/src/oauth/mod.rs
+++ crates/smbcloud-networking-account/src/oauth/mod.rs
@@ -1,3 +1,4 @@
 pub mod get_consent_url;
 pub mod get_profile;
 pub mod get_token;
+pub mod provider;
