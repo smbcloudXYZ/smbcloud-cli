a75716d03c26979a546944f9bd0f38e83c90c978
Seto <setoelkahfi@gmail.com>
2026-01-01 18:47:31 +0100

    add get_account_status endpoint

--- crates/smbcloud-networking/src/constants.rs
+++ crates/smbcloud-networking/src/constants.rs
@@ -12,6 +12,7 @@ pub const SMB_USER_AGENT: &str = "smbcloud-cli";
 
 // Paths
 pub const PATH_USERS_CHECK_EMAIL: &str = "v1/check_email";
+pub const PATH_ACCOUNT_STATUS: &str = "v1/account_status";
 pub const PATH_USERS_ME: &str = "v1/me";
 pub const PATH_USERS: &str = "v1/users";
 pub const PATH_USERS_PASSWORD: &str = "v1/users/password";
