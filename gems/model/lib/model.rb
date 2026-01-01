9cb221e1c9e4dfcfb8ed56be4bb931dfea82fb3f
Seto <setoelkahfi@gmail.com>
2026-01-01 18:01:04 +0100

    fix error codes

--- gems/model/lib/model.rb
+++ gems/model/lib/model.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative "model/version"
-require_relative "model/model"
+require_relative 'model/version'
+require_relative 'model/model'
 
 module Model
   class Error < StandardError; end
